cb6feec5ac078fc2b1709b4c2c9babb8d4b667f0
francoEmunoz <munozfranco011@gmail.com>
2023-03-29 22:24:56 -0300

    vistas

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts  import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def details(request, id):
+    post = Post.objects.get(id= id)
+    return render(request, 'blog/details.html', {'post': post})
